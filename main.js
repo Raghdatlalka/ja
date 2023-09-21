303b950e0a6908602b9e5f609ea9b4177f4d6705
Raghdatlalka <ragda.altlalka37@gmail.com>
2023-09-21 14:08:16 +0300

    q2

---
+++ main.js
@@ -0,0 +1,48 @@
+/*--Delete the wrong answers--
+1. Which of the following is/are strings?
+c) '4'
+2. Which of the following is/are numbers?
+a) 4
+
+3. Which of the following is/are booleans?
+a) true
+
+4. What is the result of 80 + 71.2?
+
+c) 8071.2
+5. What is the result of "80" + 71.2?
+a) 151.2
+6. Does 100 + 30 produce a number or a string?
+a) number
+7. Does "100" + 30 produce a number or a string?
+b)string
+*/
+//------Submit your answers under the
+//question------
+//create three different ways to declare variables
+//1.Can be redeclared
+//2.Can be reassigned a value
+//3.Only have global and function scope
+//declare a variable and reassign a value to it
+//Dim n as Integer
+//n = 1
+//create a variable and after that assign a string
+/*ypedef struct
+{
+    char name[20];
+    char surname[20];
+    int unsigned age;
+} person;
+
+with its value being: He's got it!
+/*
+1. create a variable and assign a value on how
+much a restaurant bill is.
+2. create a variable and assign a value on how
+much tax they should pay.
+3. create a variable that will calculate the bill
+* tax and its output would be: Your total bill is
+<total bill> $.
+*/
+// Round the number 50.6 to its nearest integer
+//Create a variable that is undefined
